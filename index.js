80e8ac861534b91ea62a7b060c29976ffa8bc062
Mattferreira1 <matheus2100discord@gmail.com>
2025-03-18 15:38:23 -0300

    Initial changes

---
+++ index.js
@@ -0,0 +1,19 @@
+// let doc = document.querySelector('.main_container');
+let doc = document.querySelector('html');
+let div = document.querySelector('.header_nav_list');
+
+// if(doc.computedStyleMap().get("width").value < 80){
+//     div.style.display = 'none';
+// }else{
+//     div.style.display = 'block';
+// }
+
+// while(doc.computedStyleMap().get("width").value < 1024){
+//     div.style.display = 'none';
+//     break
+// }
+// do{
+//     div.style.display = 'none';
+// }   while(doc.computedStyleMap().get("width").value < 1024)
+
+// console.log(doc.computedStyleMap().get("width").value);
